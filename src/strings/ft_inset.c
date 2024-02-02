f56fe3c68a0d437f942d0e96011d2f42234e636c
Andrew Attali <aattali@student.42.fr>
2024-02-02 14:52:06 +0100

    added getinset

--- src/strings/ft_inset.c
+++ src/strings/ft_inset.c
@@ -6,7 +6,7 @@
 /*   By: aattali <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:06:15 by aattali           #+#    #+#             */
-/*   Updated: 2023/11/13 12:06:22 by aattali          ###   ########.fr       */
+/*   Updated: 2024/02/02 14:30:10 by aattali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,15 @@ int	ft_inset(char c, const char *set)
 		if (set[i++] == c)
 			return (1);
 	return (0);
+}
+
+int	ft_getinset(char c, const char *set)
+{
+	size_t	i;
+
+	i = -1;
+	while (set[++i])
+		if (set[i] == c)
+			return (set[i]);
+	return (-1);
 }
